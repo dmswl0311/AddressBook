9b5f4a956bd5dde9e1ead5c70621f12f32a36e83
dmswl0311 <dmswl_0311@naver.com>
2021-03-11 15:25:38 +0900

    Day 7 - TextView로 추가한 연락처를 SimpleListView->SimpleAdapter으로 기능 변경 / 선택한 아이템을 삭제할 수 있게 기능 변경

--- app/src/main/java/com/cej/addressbook/AddressActivity.java
+++ app/src/main/java/com/cej/addressbook/AddressActivity.java
@@ -6,8 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,6 +18,8 @@ import org.w3c.dom.Text;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 class Address2{
     // Member Variable ----------------------------------
@@ -53,14 +58,17 @@ public class AddressActivity extends AppCompatActivity {
     // Member Variable ----------------------------------
     private final boolean D=true;
     private final String TAG="AddressActivity";
+    private int clickid=100;
 
     // View Object ----------------------------------------
     private EditText name_ed,phone_ed,email_ed;
-    private TextView addressTXT;
     private Button settingBTN;
+    private ListView listview;
+
 
     // Data --------------------------------------------------
-    private ArrayList<Address2> addressList=new ArrayList<>();
+    private ArrayList<HashMap<String,String>> addressList=new ArrayList<>();
+    private SimpleAdapter adapter;
 
     // Member Method - AppCompatActivity's override----------
     @Override
@@ -69,16 +77,12 @@ public class AddressActivity extends AppCompatActivity {
 
         // layout 보여주는
         setContentView(R.layout.address_layout);
-
-        // XML View 객체
-        name_ed=findViewById(R.id.name_ed);
-        phone_ed=findViewById(R.id.phone_ed);
-        email_ed=findViewById(R.id.email_ed);
-
-        addressTXT=findViewById(R.id.addressTXT);
-        settingBTN=findViewById(R.id.settingBTN);
+        
+        // 초기 메소드
+        init();
 
         if(D) Log.i(TAG,"onCreate()");
+
     }
     // Member Method - Onclick------------------------------
     public void onClick(View v){
@@ -89,26 +93,22 @@ public class AddressActivity extends AppCompatActivity {
                 if (name_ed.getText().length()>0 && phone_ed.getText().length()>0 && email_ed.getText().length()>0){
 
                     // (2-1)Address 객체 생성 및 ArrayList 추가
-                    addressList.add(new Address2(name_ed.getText().toString(),
-                            phone_ed.getText().toString(),
-                            email_ed.getText().toString()));
+                    HashMap<String,String> map2=new HashMap<>();
 
-                    Log.i(TAG,"add===>"+addressList.size());
+                    map2.put("name",name_ed.getText().toString());
+                    map2.put("phone",phone_ed.getText().toString());
+                    map2.put("email",email_ed.getText().toString());
+                    addressList.add(map2);
+
+                    Log.i(TAG,"addressList===>"+addressList);
 
                     // (3) edittext에 입력된 내용 삭제
                     initEXT();
 
-                    // (4) TextView 데이터 출력
-                    String data="";
-                    for (int i=0; i<addressList.size(); i++) {
-                        data += addressList.get(i).getInfo() + "\n";
-                    }
-                    if(data.length()>0){
-                        addressTXT.setText(data);
-                    }
-                    else{
-                        addressTXT.setText(R.string.nothing);
-                    }
+                    // (4) 데이터 출력
+                    adapter=new SimpleAdapter(this,addressList, R.layout.addressdata,new String[]{"name","phone","email"},new int[]{R.id.text1,R.id.textt2,R.id.text3});
+                    listview.setAdapter(adapter);
+                    Toast.makeText(this, "추가 완료", Toast.LENGTH_SHORT).show();
                 }
 
                 else{
@@ -122,21 +122,31 @@ public class AddressActivity extends AppCompatActivity {
                 // 가장 최근에 추가한 데이터 삭제
                 if (addressList.size()>0) {
 
-                    Log.i(TAG,"delete 버튼 클릭");
+                    // 삭제 할 item 선택
+                    listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            clickid=(int)id;
+                        }
+                    });
 
-                    addressList.remove(addressList.size() - 1);
-
-                    // TextView 데이터 출력
-                    String datas = "";
-                    for (int i = 0; i < addressList.size(); i++){
-                        datas += addressList.get(i).getInfo() + "\n";
+                    if (clickid==100){
+                        // 아이템이 선택되지 않았을 경우
+                        Toast.makeText(this, "삭제할 아이템을 선택해주세요!", Toast.LENGTH_SHORT).show();
                     }
-                    addressTXT.setText(datas);
+                    else{
+                        addressList.remove(clickid);
+
+                        // 데이터 출력
+                        adapter=new SimpleAdapter(this,addressList, R.layout.addressdata,new String[]{"name","phone","email"},new int[]{R.id.text1,R.id.textt2,R.id.text3});
+                        listview.setAdapter(adapter);
+                    }
                 }
                 else{
-                    Toast.makeText(this, R.string.nothing, Toast.LENGTH_LONG).show();
+                    Toast.makeText(this, "삭제할 목록이 없습니다.", Toast.LENGTH_SHORT).show();
                 }
                 break;
+                
             case R.id.settingBTN:
                 Intent MoveINT=new Intent(AddressActivity.this,SettingActivity.class);
                 startActivity(MoveINT);
@@ -151,5 +161,28 @@ public class AddressActivity extends AppCompatActivity {
         name_ed.setText("");
         phone_ed.setText("");
         email_ed.setText("");
+    }
+    private void init(){
+        // XML View 객체
+        name_ed=findViewById(R.id.name_ed);
+        phone_ed=findViewById(R.id.phone_ed);
+        email_ed=findViewById(R.id.email_ed);
+        settingBTN=findViewById(R.id.settingBTN);
+        
+        listview=findViewById(R.id.listview);
+        
+        // ArrayList 초기 데이터
+        HashMap<String,String> map=new HashMap<>();
+
+        map.put("name","Cho");
+        map.put("phone","010-9136-6343");
+        map.put("email","dmswl_0311@naver.com");
+        addressList.add(map);
+
+        adapter=new SimpleAdapter(this,addressList, R.layout.addressdata,new String[]{"name","phone","email"},new int[]{R.id.text1,R.id.textt2,R.id.text3});
+
+        listview.setAdapter(adapter);
+
+
     }
 }
